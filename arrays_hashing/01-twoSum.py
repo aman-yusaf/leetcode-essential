04d6187ba60f07336d64f1e2c76f958ef253f5db
aman.yusaf <amanjunaidyusuf@gmail.com>
2025-07-06 20:39:13 +0530

    feat(arras_hashing): twoSum

---
+++ arrays_hashing/01-twoSum.py
@@ -0,0 +1,17 @@
+from typing import List
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        """
+        Returns the indices of the two numbers such that they add up to the target.
+        """
+        hashmap = {}
+        for index, num in enumerate(nums):
+            complement = target - num
+            if complement in hashmap:
+                return [hashmap[complement], index]
+            hashmap[num] = index
+
+        # Optional: fallback return for robustness
+        return []
+
